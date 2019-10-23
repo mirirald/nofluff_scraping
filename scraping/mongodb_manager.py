d980acaf7bae8f1f792eb111d24489a60b13fc92
mirirald <mirirald@gmail.com>
2019-10-23 23:09:53 +0200

    Init MongodbManager class

---
+++ scraping/mongodb_manager.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+
+
+class MongodbManager:
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.client = MongoClient(host, port)
+
+    def connect_to_nofluff(self):
+        db = self.client.nofluff
+        return db
+
+    def get_list_existing_jobs_ids(self):
+        collection = self.connect_to_nofluff().jobs
+        ids = collection.find({}).distinct()
+        return ids
+
+    def load_in_mongodb(self, jobs_details):
+        collection = self.connect_to_nofluff().jobs
+        for job in jobs_details:
+            collection.insert_one(job)
+
+    def clean_jobs(self):
+        pass
+
+    def compare_jobs(self):
+        pass
+
+    def archive_jobs(self):
+        pass
+
+
