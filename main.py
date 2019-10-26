995e14733ee265cf3d7c66e744f6ad3de5edbc92
mirirald <mirirald@gmail.com>
2019-10-26 21:11:38 +0200

    Testing clean_json and update in mongodb

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 from scraping.mongodb_manager import *
-from scraping.scraper import *
 
-scraper = Scraper()
 client = MongodbManager('localhost', 27017)
 
-raw_list = scraper.get_raw_list_all_jobs()
-unique_ids = get_list_unique_ids(raw_list)
-ids_nofluff = unique_ids
+json_job = client.connect_to_nofluff().jobs.find_one()
 
-print(ids_nofluff)
+# print(json_job['basics']['seniority'])
 
-ids_mongodb = client.get_list_existing_jobs_ids()
+clean_json_job = dict()
+clean_json_job["company"] = dict([
+    ("name", json_job["company"]["name"]),
+    ("size", json_job["company"]["size"])
+])
+clean_json_job["id"] = json_job["id"]
+clean_json_job["basics"] = dict([
+    ("category", json_job["basics"]["category"]),
+    ("technology", json_job["basics"]["technology"]),
+    ("seniority", json_job["basics"]["seniority"])
+])
+clean_json_job["requirements"] = dict([
+    ("musts", [item["value"] for item in json_job["requirements"]["musts"]]),
+    ("nices", [item["value"] for item in json_job["requirements"]["nices"]])
+])
+clean_json_job["description"] = json_job["details"]["description"]
+clean_json_job["salary"] = json_job["essentials"]["salary"]
+clean_json_job["location"] = dict([
+    ("country", json_job["location"]["places"][0]["country"]["name"]),
+    ("city", json_job["location"]["places"][0]["city"]),
+    ("remote", json_job["location"]["remote"])
+])
+clean_json_job["benefits"] = json_job["benefits"]["benefits"]
+clean_json_job["benefits"] = clean_json_job["benefits"] + json_job["benefits"]["officePerks"]
+clean_json_job["status"] = json_job["status"]
 
-print(ids_mongodb)
+print(clean_json_job)
 
-compared_ids = compare_jobs_ids(ids_nofluff,ids_mongodb)
+id_to_update = 'AHLTAFOD'
+collection = client.connect_to_nofluff().jobs
+collection.update_one({'id': id_to_update}, {"$set": {"status": "ARCHIVED"}}, upsert=False)
 
-print('To archived : {0}'.format(compared_ids[0]))
-print('New jobs : {0}'.format(compared_ids[1]))
-
-# from full json to cleaner and flatter one
-# need to select info by name
