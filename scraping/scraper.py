59158da076660eb3bdb171e1da2d46884ed1ec66
mirirald <mirirald@gmail.com>
2019-10-26 21:13:29 +0200

    added clean_job function

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -11,6 +11,36 @@ def get_list_unique_ids(raw_list):
     return unique_ids
 
 
+def clean_job(json_job):
+    clean_json_job = dict()
+    clean_json_job["company"] = dict([
+        ("name", json_job["company"]["name"]),
+        ("size", json_job["company"]["size"])
+    ])
+    clean_json_job["id"] = json_job["id"]
+    clean_json_job["basics"] = dict([
+        ("category", json_job["basics"]["category"]),
+        ("technology", json_job["basics"]["technology"]),
+        ("seniority", json_job["basics"]["seniority"])
+    ])
+    clean_json_job["requirements"] = dict([
+        ("musts", [item["value"] for item in json_job["requirements"]["musts"]]),
+        ("nices", [item["value"] for item in json_job["requirements"]["nices"]])
+    ])
+    clean_json_job["description"] = json_job["details"]["description"]
+    clean_json_job["salary"] = json_job["essentials"]["salary"]
+    clean_json_job["location"] = dict([
+        ("country", json_job["location"]["places"][0]["country"]["name"]),
+        ("city", json_job["location"]["places"][0]["city"]),
+        ("remote", json_job["location"]["remote"])
+    ])
+    clean_json_job["benefits"] = json_job["benefits"]["benefits"]
+    clean_json_job["benefits"] = clean_json_job["benefits"] + json_job["benefits"]["officePerks"]
+    clean_json_job["status"] = json_job["status"]
+
+    return clean_json_job
+
+
 class Scraper:
 
     def __init__(self):
@@ -31,7 +61,7 @@ class Scraper:
     def get_jobs_details(self, unique_ids):
         jobs_details = []
         for i, job_id in enumerate(unique_ids, 1):
-            jobs_details.append(self.get_details_job(job_id))
+            jobs_details.append(clean_job(self.get_details_job(job_id)))
 
             # pacing
             if i % 30 == 0:
